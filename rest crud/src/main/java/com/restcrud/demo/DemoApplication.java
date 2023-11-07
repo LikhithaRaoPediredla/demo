dd7572877e8db1cbc6e93336801ea8257ce16ac7
likhithaaaaaaa <likhitha3118@gmail.com>
2023-11-07 18:32:14 +0530

    new branch demo Signed-off-by: likhithaaaaaaa <likhitha3118@gmail.com>

--- rest crud/src/main/java/com/restcrud/demo/DemoApplication.java
+++ rest crud/src/main/java/com/restcrud/demo/DemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	System.out.println("changinggit")
+	System.out.println("changinggit");
+	System.out.println("made new branch");
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
